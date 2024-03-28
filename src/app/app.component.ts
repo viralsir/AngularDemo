f44455ab2273c44fe8fc4d37b35463b6f6351561
ViralSir <viralsir2018@gmail.com>
2024-03-28 13:33:13 +0530

    component has been implemented.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,14 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
+  selector: 'home-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'AngularDemo';
+  title = 'Angular';
+  age:number = 23;
+  address:string="Naranpura";
+  no1=23;
+  no2=34;
 }
